a230ad2c2c9eda081c8ee1a445d7aa0a15781d44
sanix <sanicksikani@gmail.com>
2021-04-10 17:24:22 +0300

    Enhancing some parts

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"html/template"
 	"log"
 	"net/http"
@@ -19,10 +20,13 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.templ.Execute(w, nil)
+	t.templ.Execute(w, r)
 }
 
 func main() {
+
+	var addr = flag.String("addr", ":8000", "The addr of the application.")
+	flag.Parse() //parse the flags
 	r := newRoom()
 	http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/room", r)
@@ -30,7 +34,8 @@ func main() {
 	//go keyword allows our main goroutine to run the web server
 	go r.run()
 	//start the web server
-	if err := http.ListenAndServe(":8000", nil); err != nil {
+	log.Println("Starting web server on", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
 
